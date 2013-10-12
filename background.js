f59d69d0ff333f862bb6514230de076321f3c770
Marshall Farrier <info@marshallfarrier.com>
2013-10-12 04:43:30 -0700

    Use list of stocks to corresponding to tabs.

--- background.js
+++ background.js
@@ -1,3 +1,14 @@
+var prefix = 'http://finance.yahoo.com/echarts?s=',
+    middle = '+Interactive#symbol=',
+    suffix = ';range=1y;compare=;indicator=sma+volume+rsi;charttype=ohlc;crosshair=cross;' + 
+           'ohlcvalues=1;logscale=on;source=undefined;',
+    stocks = ['spwr', 'cree', 'eog', 'ddd'];
+
 chrome.browserAction.onClicked.addListener(function() {
-    chrome.tabs.create({url: 'http://www.google.com'});
+    var _url;
+
+    for (var i = 0; i < stocks.length; i++) {
+        _url = prefix + stocks[i] + middle + stocks[i] + suffix;
+        chrome.tabs.create({url: _url});
+    }
 });
