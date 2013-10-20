4cb00a19230db2c3dbb24caaaa4ee48014fef51b
Marshall Farrier <info@marshallfarrier.com>
2013-10-19 20:47:47 -0700

    Add callback to sortable to prepare db connection.

--- scripts/options.js
+++ scripts/options.js
@@ -2,8 +2,18 @@ var updateUrlView = new multiTab.UpdateUrlView( {el: $('#update-and-delete-urls'
     insertUrlView = new multiTab.InsertUrlView({
         el: $('#insert-url'),
         updateView: updateUrlView
-    });
+    }),
+    updateIndices = function(event, ui) {
+        //disable sorting with:
+        //$('#url-tbody').sortable("option", "disabled", true);
+        // Then enable again when database has been updated.
+        console.log('sorting!');
+    };
 
 $(function() {
-    $('#url-tbody').sortable();
+    // Cf. http://api.jqueryui.com/sortable/#event-stop
+    // http://jsfiddle.net/pmw57/tzYbU/205/
+    $('#url-tbody').sortable({
+        stop: updateIndices
+    });
 });
